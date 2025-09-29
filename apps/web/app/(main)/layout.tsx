317562277ff6491d08cc3ee94ac56662ab49356f
Pondws <watcharamai13@gmail.com>
2025-09-29 15:40:05 +0700

    fix: delete console log

--- apps/web/app/(main)/layout.tsx
+++ apps/web/app/(main)/layout.tsx
@@ -9,7 +9,6 @@ import {
 import { Header } from "components"
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-  console.log('children', children)
   return (
     <SidebarProvider>
       <Sidebar />
